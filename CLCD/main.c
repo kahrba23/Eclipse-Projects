cf12c80acbc8c7d33a14c07a8433977b445854a4
kahrba23 <ahmedkhrba33@gmail.com>
2023-02-13 04:33:06 +0200

    CLCD

--- CLCD/main.c
+++ CLCD/main.c
@@ -1,6 +1,7 @@
 #include "STD_TYPES.h"
 #include"CLCD_interface.h"
 #include"DIO_interface.h"
+#include <util/delay.h>
 void main(){
 	DIO_u8SetPortDirection(DIO_u8PORTD,DIO_u8PORT_OUTPUT);
 	DIO_u8SetPinDirection(DIO_u8PORTC,DIO_u8PIN0,DIO_u8PIN_OUTPUT);
@@ -8,11 +9,23 @@ void main(){
 	DIO_u8SetPinDirection(DIO_u8PORTC,DIO_u8PIN2,DIO_u8PIN_OUTPUT);
 	CLCD_voidInit();
 	u8 Local_u8Arr[]={0b00000001,0b00000011,0b00000000,0b00000001,0b00000001,0b00000001,0b00000001,0b00000001};
-	CLCD_voidWriteSpecialCharacter(Local_u8Arr,0,1,8);
+	u8 Local_u8inc1=0,Local_u8inc2=3;
+
 	while(1)
 	{
+		CLCD_voidGoToXY(0,Local_u8inc1);
+		CLCD_voidSendString("AYAT");
+		_delay_ms(2000);
+		if(Local_u8inc1<12) Local_u8inc1+=6;
+		else Local_u8inc1=0;
+		CLCD_voidSendCommand(1);
+		CLCD_voidGoToXY(1,Local_u8inc2);
+		CLCD_voidSendString("AYAT");
+		_delay_ms(2000);
+		if(Local_u8inc2<15) Local_u8inc2+=6;
+		else Local_u8inc2=3;
+		CLCD_voidSendCommand(1);
 
 	}
 
 }
-
